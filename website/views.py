26a6ebf760d5c93ed2a682af9d082605794ffa66
Yaroslav Sh <yarikshovk2004@gmail.com>
2025-11-28 13:54:26 +0200

    commit before power cut

--- website/views.py
+++ website/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    return render(request, 'website/home.html', {})
+    if request.method == 'post':
+            username = request.post['username']
+            password = request.post['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                 login(request, user)
+                 messages.success(request, "LogIn successful!")
+                 return redirect('home')
+            else:
+                 messages.error(request, "Something went wrong, try again")
+                 redirect('home')
+
+    else:
+        return render(request, 'website/home.html', {})
 
 
 def logout_user(request):
